8953990c16530c0eb6940b18a1e65fbb487a89c5
komalgt <51704005+komalgt@users.noreply.github.com>
2025-10-29 18:09:54 +0530

    Update LambdaDeployer.groovy

--- src/org/example/LambdaDeployer.groovy
+++ src/org/example/LambdaDeployer.groovy
@@ -1,21 +1,33 @@
-/**
- * Deploy to AWS ECS via shared library.
- * Usage:
- *   deployECS(
- *     cluster: 'my-cluster',
- *     service: 'my-service',
- *     image: 'myrepo/myimage:tag',
- *     awsRegion: 'us-east-1',
- *     awsCredentialsId: 'aws-creds'
- *   )
- */
-def call(Map config = [:]) {
-    def deployer = new org.example.ECSDeployer(this)
-    deployer.deploy(
-        cluster: config.cluster,
-        service: config.service,
-        image: config.image,
-        awsRegion: config.awsRegion,
-        awsCredentialsId: config.awsCredentialsId
-    )
+package org.example
+
+class LambdaDeployer implements Serializable {
+    def steps
+
+    LambdaDeployer(steps) {
+        this.steps = steps
+    }
+
+    /**
+     * Deploys code to an AWS Lambda function.
+     * Usage params: functionName, s3Bucket, s3Key, awsRegion, awsCredentialsId
+     */
+    def deploy(Map config) {
+        steps.withCredentials([steps.usernamePassword(
+            credentialsId: config.awsCredentialsId,
+            usernameVariable: 'AWS_ACCESS_KEY_ID',
+            passwordVariable: 'AWS_SECRET_ACCESS_KEY'
+        )]) {
+            steps.sh """
+                export AWS_ACCESS_KEY_ID=\$AWS_ACCESS_KEY_ID
+                export AWS_SECRET_ACCESS_KEY=\$AWS_SECRET_ACCESS_KEY
+                export AWS_DEFAULT_REGION=${config.awsRegion}
+
+                aws lambda update-function-code \\
+                  --function-name ${config.functionName} \\
+                  --s3-bucket ${config.s3Bucket} \\
+                  --s3-key ${config.s3Key}
+            """
+        }
+        steps.echo "Deployment to Lambda function '${config.functionName}' in region '${config.awsRegion}' initiated."
+    }
 }
